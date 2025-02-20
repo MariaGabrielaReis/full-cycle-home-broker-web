55765403a05dcb5c5f92027dbec8d86ec49b584a
MariaGabrielaReis <mariagabrielagreis@gmail.com>
2025-02-20 09:31:14 -0300

    style: add screen padding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="h-screen flex flex-col">{children}</body>
+      <body className="h-screen flex flex-col p-10">{children}</body>
     </html>
   );
 }
