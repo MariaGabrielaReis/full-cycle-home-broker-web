1e225572a54574ef204a6dc1b964dde78e013b93
MariaGabrielaReis <mariagabrielagreis@gmail.com>
2025-02-20 10:41:12 -0300

    chore: compile all requests in one file

---
+++ src/queries/queries.tsx
@@ -0,0 +1,24 @@
+export async function getAssets(): Promise<Asset[]> {
+  const response = await fetch(`http://localhost:3333/assets`);
+  return response.json();
+}
+
+export async function getAsset(ticker: string): Promise<Asset> {
+  const response = await fetch(`http://localhost:3333/assets/${ticker}`);
+  return response.json();
+}
+
+export async function getWallet(walletId: string): Promise<Wallet | null> {
+  const response = await fetch(`http://localhost:3333/wallets/${walletId}`);
+
+  if (!response.ok) return null;
+
+  return response.json();
+}
+
+export async function getOrders(walletId: string): Promise<Order[]> {
+  const response = await fetch(
+    `http://localhost:3333/orders?walletId=${walletId}`,
+  );
+  return response.json();
+}
